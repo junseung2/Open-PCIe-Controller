e5977c9e96570e3b331fdafbc1a0fe72eeedf478
leejs <dlwnstmd2021@naver.com>
2024-05-25 12:44:20 +0900

    cleaning

--- 2.RX/0.PCIe_TL_RX/tb/csrc/uvm_verdi_dpi.d
+++
@@ -1,8 +0,0 @@
-uvm_verdi_dpi.o: \
- /usr/synopsys/vcs/R-2020.12-SP1-1/etc/uvm-1.2/verdi/dpi/uvm_verdi_dpi.cpp \
- /usr/synopsys/vcs/R-2020.12-SP1-1/include/vpi_user.h \
- /usr/synopsys/vcs/R-2020.12-SP1-1/include/veriuser.h \
- /usr/synopsys/vcs/R-2020.12-SP1-1/include/sv_vpi_user.h \
- /usr/synopsys/vcs/R-2020.12-SP1-1/include/vpi_user.h \
- /usr/synopsys/vcs/R-2020.12-SP1-1/include/vcs_vpi_user.h \
- /usr/synopsys/vcs/R-2020.12-SP1-1/include/sv_vpi_user.h
